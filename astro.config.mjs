7c408a3e8d2ce85e89637187958edcccf1270801
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2026-02-24 21:26:34 -0600

    feat: subit sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
+const SITE = 'https://task-goblin.netlify.app';
+
 export default defineConfig({
-  integrations: [react()],
+  site: SITE,
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
