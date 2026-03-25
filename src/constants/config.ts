7b9c46354feacf254ea10a190821de09aff38908
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2026-03-25 01:08:10 -0600

    feat: habilitar pagos

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,5 +6,5 @@ export const APP_CONFIG = {
    * Flag to enable or disable the licensing/payment system.
    * When false, users will not be able to purchase or obtain new licenses.
    */
-  ENABLE_LICENSING: false,
+  ENABLE_LICENSING: true,
 };
